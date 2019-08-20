4e6c2fc41ac29ec13f205c20afd1cb71bbb5f7aa
wudong <wudong@uinnova.com>
2019-08-20 22:24:17 +0800

    <feat>(all): <基础版本代码>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './routers'
+import store from './stores'
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.prototype.axios = axios;
+Vue.config.productionTip = false;
+
+Vue.filter('setWH',(url,arg) => {
+    return url.replace(/w\.h/,arg);
+})
 
 new Vue({
   router,
